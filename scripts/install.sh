28651f7ecdf37e4f2ad0ac9a5352c706a4214aad
Russel Zac Gauthier <gauthier.russel@gmail.com>
2018-01-30 21:16:54 -0600

    -1.18 - Added scripts/install.sh (must be updated for full NodeJS install

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##
+##
+## Author: Russel Gauthier(c) - GPLv3 - Arxos - v1.17
+##
+##
+
+#
+# Install Compass & Susy for node_modules & grunt to work
+#
+sudo gem install compass;
+sudo gem install susy;
